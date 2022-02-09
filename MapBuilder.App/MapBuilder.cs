44211ce0cabd4bdb10233675f2173ddc906f4a45
Konstantin Didenko <ironbullyk@gmail.com>
2022-02-09 23:37:02 +0200

    Pure task

---
+++ MapBuilder.App/MapBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapBuilder.App
+{
+    public class MapBuilder
+    {
+        private SignStep _currentOfficeManagerPosition = new()
+        {
+            Floor = 0,
+            Section = 1
+        };
+
+        public IEnumerable<string> BuildRouteMap(IEnumerable<SignStep> signatureMap)
+        {
+        }
+
+    }
+}
---
+++ MapBuilder.App/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MapBuilder.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
